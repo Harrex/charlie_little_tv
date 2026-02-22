77ae81f5647be9c1f01138569d5157f254c98999
Hazel Upston <harrek@Hazels-MacBook-Pro.local>
2026-02-22 13:48:57 +1100

    Initial

---
+++ README.md
@@ -0,0 +1,1 @@
+# charlie_little_tv
---
+++ script.py
@@ -0,0 +1,57 @@
+import cv2
+import sys
+
+static = cv2.VideoCapture("static.mp4")
+Audrey = cv2.VideoCapture("Audrey.mp4")
+Harinee = cv2.VideoCapture("Harinee.mp4")
+Heidi = cv2.VideoCapture("Heidi.mp4")
+cv2.namedWindow("Video", cv2.WND_PROP_FULLSCREEN)
+cv2.setWindowProperty("Video", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+
+def static_loop(cap):
+    while True:
+        ret, frame = cap.read()
+
+        if not ret:
+            cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+
+        cv2.imshow("Video", frame)
+
+        # Press Q to quit
+        if cv2.waitKey(25) & 0xFF == ord("q"):
+            return
+
+
+def video_play(cap):
+    while True:
+        ret, frame = cap.read()
+
+        if not ret:
+            break  # No more frames → end of video
+
+        cv2.imshow("Video", frame)
+
+        # Press Q to quit
+        if cv2.waitKey(25) & 0xFF == ord("q"):
+            return
+    static_loop(static)
+
+
+if sys.argv[1] == "Audrey":
+    a = Audrey
+else:
+    a = Harinee
+
+
+try:
+    static_loop(static)
+    while True:
+        video_play(a)
+        video_play(Heidi)
+except KeyboardInterrupt:
+    print("Stopping...")
+    Audrey.release()
+    Harinee.release()
+    Heidi.release()
+    cv2.destroyAllWindows()
